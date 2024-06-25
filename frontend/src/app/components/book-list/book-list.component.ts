621ef942f3889b7939fd5570aa38ff5931e47211
Emir Mehmedćehajić <emir.mehmedcehajic@edu.fit.ba>
2024-06-26 00:34:09 +0200

    displaying books in grid

--- frontend/src/app/components/book-list/book-list.component.ts
+++ frontend/src/app/components/book-list/book-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model'
+import {BookItemComponent} from "../book-item/book-item.component";
 
 @Component({
   selector: 'app-book-list',
-  standalone: true,
-  imports: [],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  standalone: true,
+  imports: [
+    BookItemComponent
+  ],
+  styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+    });
+  }
 }
--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-interface Book {
-  id: number;
-  title: string;
-  author: string;
-  publicationYear: number;
-  description: string;
-  coverImage: string;
-  liked: boolean;
-}
+import { Book } from '../models/book.model'
 
 @Injectable({
   providedIn: 'root'
