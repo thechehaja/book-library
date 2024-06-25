22506b47853ee274e7ff6d5dc2e3da240d91ef23
Emir Mehmedćehajić <emir.mehmedcehajic@edu.fit.ba>
2024-06-25 23:53:00 +0200

    initial components-services

---
+++ frontend/src/app/components/book-modal/book-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-modal.component.html',
+  styleUrl: './book-modal.component.css'
+})
+export class BookModalComponent {
+
+}
