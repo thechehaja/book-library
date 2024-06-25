e979b8641ac84eb1241f5c821d5fdcb9d2bdc462
Emir Mehmedćehajić <emir.mehmedcehajic@edu.fit.ba>
2024-06-26 00:11:27 +0200

    api connection - book interface

--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  publicationYear: number;
+  description: string;
+  coverImage: string;
+  liked: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private apiUrl = 'https://freetestapi.com/api/v1/books';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
 }
